b0ef7a223e9732165ffc345b267c7e30060fc686
lololchike <lucyololchike@gmail.com>
2023-06-24 21:22:10 +0300

    update of hero squad classs

--- src/main/java/controllers/HeroController.java
+++ src/main/java/controllers/HeroController.java
@@ -1,4 +1,39 @@
-package controllers;
+ppackage com.lololchike.controllers;
+
+import com.lololchike.dao.HeroDao;
+import com.lololchike.model.Hero;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import com.google.gson.Gson;
 
 public class HeroController {
+
+    private final HeroDao heroDao;
+
+    public HeroController(HeroDao heroDao) {
+        this.heroDao = heroDao;
+    }
+
+    public Route getAllHeroes() {
+        return (Request req, Response res) -> {
+            res.type("application/json");
+            return new Gson().toJson(heroDao.getAllHeroes());
+        };
+    }
+
+    public Route addHero() {
+        return (Request req, Response res) -> {
+            String name = req.queryParams("name");
+            int age = Integer.parseInt(req.queryParams("age"));
+            String power = req.queryParams("power");
+            String weakness = req.queryParams("weakness");
+
+            Hero hero = new Hero(name, age, power, weakness);
+            heroDao.addHero(hero);
+
+            res.status(201);
+            return "Hero added successfully";
+        };
+    }
 }
--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,4 +1,30 @@
-package controllers;
+package com.lololchike.controllers;
 
+import com.lololchike.dao.HeroDao;
+import com.lololchike.dao.SquadDao;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
 public class HomeController {
+
+    private final HeroDao heroDao;
+    private final SquadDao squadDao;
+
+    public HomeController(HeroDao heroDao, SquadDao squadDao) {
+        this.heroDao = heroDao;
+        this.squadDao = squadDao;
+    }
+
+    public Route getHome() {
+        return (Request req, Response res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("heroes", heroDao.getAllHeroes());
+            model.put("squads", squadDao.getAllSquads());
+            return new ModelAndView(model, "index.hbs");
+        };
+    }
 }
